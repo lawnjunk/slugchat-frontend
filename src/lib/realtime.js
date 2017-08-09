6055a601ffbf97044b6c12779c4823789057582f
Duncan Marsh <dwolfm@gmail.com>
2017-08-09 00:00:01 -0700

    added slugchat-frontend

---
+++ src/lib/realtime.js
@@ -0,0 +1,23 @@
+import io from 'socket.io-client'
+import * as util from './util.js'
+
+export const socket = io(`${__API_URL__}`)
+
+export const initSubscribers = (store) => (...subscribers) => {
+  let handlers = Object.assign(...subscribers) 
+  Object.keys(handlers)
+  .map(type => ({type, handler: handlers[type]}))
+  .forEach(event => {
+    socket.on(event.type, (data) => {
+      util.log('__EVENT__', event.type, data)
+      event.handler({
+        // overwrite dispatch so that data coming from the server 
+        // wont get re-dispatched to the server in the redux-realtime
+        dispatch: (action) => store.dispatch({blockRealtime: true, ...action}),
+        getState: store.getState,
+      })(socket)(data)
+    })
+  })
+}
+
+
